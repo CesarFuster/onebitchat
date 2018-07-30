326c186077d5ad896b9e7c96d09bb70d1b852908
cesarfuster <cfuster@uol.com.br>
2018-07-30 09:56:50 -0300

    Teams Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'teams/index'
+
+  get 'teams/show'
+
+  get 'teams/create'
+
+  get 'teams/destroy'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
