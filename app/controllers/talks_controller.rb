905ccdcb97d47dba66fc23dba263ed1d66a8e4a1
cesarfuster <cfuster@uol.com.br>
2018-07-31 07:45:02 -0300

    Talks Controller fixed

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -10,4 +10,4 @@ class TalksController < ApplicationController
   def set_talk
     @talk = Talk.find_by(user_one_id: [params[:id], current_user.id], user_two_id: [params[:id], current_user.id], team: params[:team_id])
   end
-
+end
